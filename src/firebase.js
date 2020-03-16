d8b37e9ff0f9a73ba9a7956fb56807d42a3cc504
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-03-16 16:02:38 -0700

    #3 Created Semantic-ui Register component

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+ import firebase from 'firebase/app'
+ import 'firebase/auth'
+ import 'firebase/database'
+ import 'firebase/storage'
+
+ 
+ 
+ 
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDXO_KEL6i-05WnqAqpXlZ301-Hl26TbQA",
+    authDomain: "slack-1d4e5.firebaseapp.com",
+    databaseURL: "https://slack-1d4e5.firebaseio.com",
+    projectId: "slack-1d4e5",
+    storageBucket: "slack-1d4e5.appspot.com",
+    messagingSenderId: "479166501385",
+    appId: "1:479166501385:web:fed5f3d6e4afadbd769ed8",
+    measurementId: "G-FVX9SYSGHL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export default firebase;

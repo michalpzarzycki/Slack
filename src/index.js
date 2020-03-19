ccf9b1f2b98009b6e0768cc42d421cadf3c5bf44
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-03-19 09:08:46 -0700

    #9 Added withRouter to change the url path if there is a logged in user

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import 'semantic-ui-css/semantic.min.css'
+import firebase from './firebase'
 
 
-const Root = () => (
-    <Router>
+const Root = (props) => {
+    useEffect(()=>{
+        console.log("EK")
+        firebase.auth().onAuthStateChanged(user => {
+            if(user) {
+                props.history.push('/')
+            }
+        })
+    }, [])
+
+    return(
+
         <Switch>
             <Route exact path="/" component={App}/>
             <Route path="/login" component={Login}/>
             <Route path="/register" component={Register}/>
         </Switch>
-    </Router>
-)
+    )
+}
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const RootWithRouter = withRouter(Root)
+ReactDOM.render(<Router><RootWithRouter /></Router>, document.getElementById('root'));
 
 
 serviceWorker.unregister();

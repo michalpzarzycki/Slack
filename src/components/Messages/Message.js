35375015533273ff674991c2fde4d8a658dc72f1
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-03-23 14:40:28 -0700

    #25 displaying messages

---
+++ src/components/Messages/Message.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Comment } from 'semantic-ui-react'
+
+const Message = ({ key, message, user}) => {
+    
+    const isOwnMessage = (message, user) => {
+        return message.user.id === user.uid ? 'message__self' : ''
+    }
+
+    return (
+   <Comment>
+       <Comment.Avatar src={message.user.avatar}/>
+       <Comment.Content className={isOwnMessage(message, user)}>
+            <Comment.Author as="a">{message.user.name}</Comment.Author>
+            <Comment.Metadata>{message.timestamp}</Comment.Metadata>
+            <Comment.Text>{message.content}</Comment.Text>
+       </Comment.Content>
+   </Comment>
+    )
+}
+
+export default Message
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,18 +1,48 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import MessageHeader from './MessageHeader';
 import MessageForm from './MessageForm';
-import { Segment, Comment, Message } from 'semantic-ui-react';
+import { Segment, Comment } from 'semantic-ui-react';
 import firebase from '../../firebase';
+import Message from './Message'
 
 const Messages = ({ currentChannel, currentUser }) => {
 const [messageRef, setMessageRef] = useState(firebase.database().ref("messages"))
 const [channel, setChannel] = useState(currentChannel)
+const [user, serUser] = useState(currentUser)
+const [messages, setMessages] = useState([])
+const [messagesLoading, setMessagesLoading] = useState(false)
+
+useEffect(() => {
+    if(channel && user) {
+        addListener(channel.id)
+    }
+}, [])
+
+const addListener = (channelId) => {
+    let loadedMessages = [];
+    messageRef.child(channelId).on('child_added', snap => {
+        loadedMessages.push(snap.val())
+        setMessages([...loadedMessages])
+        setMessagesLoading(false)
+    })
+}
+
+const displayMessages = (messages) => (
+   messages.length > 0 && messages.map(message => (
+      <Message
+        key={message.timestamp}
+        message={message}
+        user={user}
+      />
+  ))
+  
+)
     return(
         <>
         <MessageHeader />
         <Segment>
             <Comment.Group className="messages">
-                {/* MESSAGES */}
+                {displayMessages(messages)}
             </Comment.Group>
         </Segment>
         <MessageForm messageRef={messageRef} currentChannel={channel} currentUser={currentUser}/>
